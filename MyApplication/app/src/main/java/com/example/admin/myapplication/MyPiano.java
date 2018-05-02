edfa06d528ff544a2bbcf88d7d8b262027b58718
DESKTOP-O0E675V\admin <lkx017@gmail.com>
2018-05-02 11:01:04 +0900

    학습기능 추가 : 기존의 노래와 비교해 맞는지 확인할 수 있음

--- MyApplication/app/src/main/java/com/example/admin/myapplication/MyPiano.java
+++ MyApplication/app/src/main/java/com/example/admin/myapplication/MyPiano.java
@@ -1,6 +1,5 @@
 package com.example.admin.myapplication;
 
-
 import android.content.Intent;
 import android.graphics.Color;
 import android.media.AudioManager;
@@ -8,7 +7,8 @@ import android.media.AudioManager;
         import android.media.SoundPool;
         import android.os.Handler;
         import android.os.Message;
-        import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.util.Log;
         import android.view.Menu;
@@ -27,9 +27,15 @@ public class MyPiano extends AppCompatActivity {
     int ck = 0;
     int cnt = 0;
     int[] ar = new int[8];
+    boolean boolck = false; //노래가 눌려졌는지
+    int intck = 0; //무슨 노랜지
+    int ocnt = 0;
+
+    ArrayList<Integer> input = new ArrayList<Integer>();
+    ArrayList<String> rs = new ArrayList<String>();
 
     ArrayList<Integer[]> song1 = new ArrayList<Integer[]>();
-    int[] br = {3, 2, 1, 2, 3, 3, 3, 2, 2, 2, 3, 3, 3, 3, 2, 1, 2, 3, 3, 3, 2, 2, 3, 2, 1};
+    int[] br = {3, 2, 1, 2, 3, 3, 3, 2, 2, 2, 3, 5, 5, 3, 2, 1, 2, 3, 3, 3, 2, 2, 3, 2, 1};
     int[] cr = {750, 250, 500, 500, 500, 500, 1000, 500, 500, 1000, 500, 500, 1000, 750, 250, 500, 500, 500, 500, 1000, 500, 500, 750, 250, 2000};
 
 
@@ -63,60 +69,72 @@ public class MyPiano extends AppCompatActivity {
     final int FLY_ID = 10;
     final int STOP_ID = 20;
     final int SCHOOL_ID = 30;
+    final int CK_ID = 40;
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0,FLY_ID,1,"비행기");
-        menu.add(0,STOP_ID,3,"stop");
+        menu.add(0, FLY_ID, 1, "비행기");
+        menu.add(0, STOP_ID, 3, "stop");
         menu.add(0, SCHOOL_ID, 2, "학교종");
+        menu.add(0, CK_ID, 4, "비교");
         return super.onCreateOptionsMenu(menu);
     }
 
-   public void timer(){
+    public void timer() {
 
-       handler = new Handler() {
-           @Override
-           public void handleMessage(Message msg) {
-               if(ck==0) {
-                   handler.sendEmptyMessageDelayed(0, song1.get(cnt)[1]);
-                   pool.play(ar[song1.get(cnt)[0] - 1], 1, 1, 0, 0, 1);
-                   cnt++;
-                   if (cnt == br.length) {
-                       handler.removeMessages(0);
-                       return;
-                   }
-               } else {
-                   handler.sendEmptyMessageDelayed(0, song2.get(cnt)[1]);
-                   pool.play(ar[song2.get(cnt)[0] - 1], 1, 1, 0, 0, 1);
-                   cnt++;
-                   if (cnt == dr.length) {
-                       handler.removeMessages(0);
-                       return;
-                   }
-               }
-           }
-       };handler.sendEmptyMessage(0);
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                input.clear();
+                if (ck == 0) {
+                    handler.sendEmptyMessageDelayed(0, song1.get(cnt)[1]);
+                    checked(song1.get(cnt)[0] - 1);
+                    pool.play(ar[song1.get(cnt)[0] - 1], 1, 1, 0, 0, 1);
+                    cnt++;
+                    if (cnt == br.length) {
+                        handler.removeMessages(0);
+                        return;
+                    }
+                } else {
+                    handler.sendEmptyMessageDelayed(0, song2.get(cnt)[1]);
+                    checked(song2.get(cnt)[0] - 1);
+                    pool.play(ar[song2.get(cnt)[0] - 1], 1, 1, 0, 0, 1);
+
+                    cnt++;
+                    if (cnt == dr.length) {
+                        handler.removeMessages(0);
+                        return;
+                    }
+                }
+            }
+        };
+        handler.sendEmptyMessage(0);
     }
+
     public void stop() {
         cnt = 0;
-        if(handler!=null) {
+        if (handler != null) {
             handler.removeMessages(0);
         }
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-
         switch (item.getItemId()) {
             case 10:
-                ck=0;
-                if(handler!=null) {
+                boolck = true;
+                ck = 0;
+                intck = 1;
+                if (handler != null) {
                     stop();
                 }
                 timer();
                 break;
             case 30:
-                ck=1;
-                if(handler!=null) {
+                boolck = true;
+                intck = 2;
+                ck = 1;
+                if (handler != null) {
                     stop();
                 }
                 timer();
@@ -124,16 +142,55 @@ public class MyPiano extends AppCompatActivity {
             case 20:
                 stop();
                 break;
+            case 40:
+                if (boolck == false) {
+                } else {
+                    boolchecked(intck);
+                    new AlertDialog.Builder(this)
+                            .setTitle("정오표 " + ocnt + "/" + rs.size())
+                            .setMessage(rs.toString())
+                            .setPositiveButton("확인", null)
+                            .show();
+                }
+                boolck = false;
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
 
     public void checked(int n) {
-       for(int i=0;i<8;i++) {
-           btns[i].setBackgroundColor(Color.WHITE);
-       }
+        for (int i = 0; i < 8; i++) {
+            btns[i].setBackgroundColor(Color.WHITE);
+        }
         btns[n].setBackgroundColor(Color.YELLOW);
     }
+
+    public void boolchecked(int n) {
+        if (n == 1) {
+            rs.clear();
+            ocnt = 0;
+            for (int i = 0; i < br.length; i++) {
+                if (br[i] == input.get(i)) {
+                    rs.add("o");
+                    ocnt++;
+                } else {
+                    rs.add("x");
+                }
+            }
+        } else {
+            rs.clear();
+            ocnt = 0;
+            for (int i = 0; i < dr.length; i++) {
+                if (dr[i] == input.get(i)) {
+                    rs.add("o");
+                    ocnt++;
+                } else {
+                    rs.add("x");
+                }
+            }
+        }
+    }
+
     View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -141,34 +198,58 @@ public class MyPiano extends AppCompatActivity {
                 case R.id.btn1:
                     checked(0);
                     pool.play(ar[0], 1, 1, 0, 0, 1);
+                    if (boolck == true) {
+                        input.add(1);
+                    }
                     break;
                 case R.id.btn2:
                     checked(1);
                     pool.play(ar[1], 1, 1, 0, 0, 1);
+                    if (boolck == true) {
+                        input.add(2);
+                    }
                     break;
                 case R.id.btn3:
                     checked(2);
                     pool.play(ar[2], 1, 1, 0, 0, 1);
+                    if (boolck == true) {
+                        input.add(3);
+                    }
                     break;
                 case R.id.btn4:
                     checked(3);
                     pool.play(ar[3], 1, 1, 0, 0, 1);
+                    if (boolck == true) {
+                        input.add(4);
+                    }
                     break;
                 case R.id.btn5:
                     checked(4);
                     pool.play(ar[4], 1, 1, 0, 0, 1);
+                    if (boolck == true) {
+                        input.add(5);
+                    }
                     break;
                 case R.id.btn6:
                     checked(5);
                     pool.play(ar[5], 1, 1, 0, 0, 1);
+                    if (boolck == true) {
+                        input.add(6);
+                    }
                     break;
                 case R.id.btn7:
                     checked(6);
                     pool.play(ar[6], 1, 1, 0, 0, 1);
+                    if (boolck == true) {
+                        input.add(7);
+                    }
                     break;
                 case R.id.btn8:
                     checked(7);
                     pool.play(ar[7], 1, 1, 0, 0, 1);
+                    if (boolck == true) {
+                        input.add(8);
+                    }
                     break;
                 /*case R.id.btn01:
                     pool.play(ar[8], 1, 1, 0, 0, 1);
@@ -190,6 +271,7 @@ public class MyPiano extends AppCompatActivity {
             song1.add(note);
         }
     }
+
     public void setting_song2() {
         for (int i = 0; i < dr.length; i++) {
             Integer[] note = {dr[i], er[i]};
